df272765bdcb73b21d6979e4a76498b62359bda0
IsraelPereira <pereira.israel2070@gmail.com>
2022-06-24 08:12:21 -0300

    resolução do terceiro exercício >>>>>> utilizei a mesma lógica que aprendi no exerccício do número maior, ma aqui trabalhando com strings >>>>> a propriedade [index].length foi fundamental; na realidade uma combinação de propriedades.

--- Fundamentos/Bloco-4-introducao-Js/dia-3-Algoritmos-Logica/exercicios.js
+++ Fundamentos/Bloco-4-introducao-Js/dia-3-Algoritmos-Logica/exercicios.js
@@ -1,16 +1,29 @@
-// 1 fatorial 10
-// let n = 10
-// let resultado = 1
-// for ( let i = 9; i >= n-9; i -= 1 ) {
-//     resultado = resultado * (n * i)
-//     console.log (i);
-// }
-// console.log(resultado)
+1 fatorial 10
+let n = 10
+let resultado = 1
+for ( let i = 9; i >= n-9; i -= 1 ) {
+    resultado = resultado * (n * i)
+    console.log (i);
+}
+console.log(resultado)
 
-// 2 
+ 2 
 let word = 'tryber';
 let palavraAoContrario = "";
 for ( index = 5; index >= word.length-6; index -= 1 ) {
     palavraAoContrario += word[index];
 }
 console.log(palavraAoContrario);
+
+3
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = 'java'
+
+for (let index = 0; index < array.length; index += 1) {
+    
+    if (array[index].length > maiorPalavra.length ) {
+        maiorPalavra = array[index];
+    }
+}
+console.log(maiorPalavra);
+
