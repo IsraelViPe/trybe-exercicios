175e77b5a654891d4bf026626a275894175e68d2
IsraelPereira <pereira.israel2070@gmail.com>
2022-06-24 07:49:57 -0300

    exercicio 2 ; a novidade pra mim é que o for funciona com string também

--- Fundamentos/Bloco-4-introducao-Js/dia-3-Algoritmos-Logica/exercicios.js
+++ Fundamentos/Bloco-4-introducao-Js/dia-3-Algoritmos-Logica/exercicios.js
@@ -1,9 +1,16 @@
 // 1 fatorial 10
-let n = 10
-let resultado = 1
-for ( let i = 9; i >= n-9; i -= 1 ) {
-    resultado = resultado * (n * i)
-    console.log (i);
-}
-console.log(resultado)
+// let n = 10
+// let resultado = 1
+// for ( let i = 9; i >= n-9; i -= 1 ) {
+//     resultado = resultado * (n * i)
+//     console.log (i);
+// }
+// console.log(resultado)
 
+// 2 
+let word = 'tryber';
+let palavraAoContrario = "";
+for ( index = 5; index >= word.length-6; index -= 1 ) {
+    palavraAoContrario += word[index];
+}
+console.log(palavraAoContrario);
