55411c88d06549a3ca80d3e6e7af68585de8ca33
IsraelPereira <pereira.israel2070@gmail.com>
2022-06-26 11:53:58 -0300

    resolução restante dos exercícios da dinâmica de grupos

--- Fundamentos/Bloco-4-introducao-Js/dia-2-JS-Array-For/dinamica.js
+++ Fundamentos/Bloco-4-introducao-Js/dia-2-JS-Array-For/dinamica.js
@@ -28,75 +28,49 @@ for ( let index = 15; index <= 155; index += 1) {
 // 3. Crie um algoritmo que simula o jogo “pedra, papel e tesoura” levando em consideração  que são apenas duas pessoas jogando e imprima o resultado no formato:
 // “Jogador 1 vence” ou “Empate” ou “Jogador 2 vence”.
 
-// são doi jogadores com 3 opçoes
-// vence ou empata
 
+let j1 = "tesoura"
+let j2 = "pedra"
+let j3 = "papel"
 
+let jogador1 = j3;
+let jogador2 = j2;
 
-// let jogada1 = "pedra"
-// let jogada2 = "papel"
-// let jogada3 = "tesoura"
+if ( jogador1 == jogador2 ) {
+    console.log( "empate")
+} else if ( jogador1 == j1 && jogador2 == j3 || jogador1 == j2 && jogador2 == j1 || jogador1 == j3 && jogador2 == j2 ) {
+    console.log ("Jogador 1 vence")
+} else { console.log ("Jogador 2 vence")}
 
-// let jogador1 =[jogada1,jogada2,jogada3];
-// let jogador2 =[jogada1,jogada2,jogada3];
-
-
-
-// console.log (jogador1);
-
-// for ( let index2= 0; index2 < jogador2.length; index2 +=1) {
-   
-  
-// }
-// let jogo = ['Pedra', 'Papel', 'Tesoura'];
-// let jogador1 = jogo[Math.floor(Math.random() * 2)];
-// let jogador2 = jogo[Math.floor(Math.random() * 2)];
-
-// console.log (jogador1, jogador2);
-
-
-
-
-// const n = 9
-
-// let result = 100;
-
-// for ( let i =0; i <= n; i += 1){
-//     result -= i 
-//     console.log(i);
-// }
-// console.log (result);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+////////RESOLUÇÃO COM FUNÇÃO//////
 
 
 // 4. Desenvolva um algoritmo que verifica se a pessoa é maior ou menor de idade.
 // Imprima no console seguindo o exemplo: “A pessoa é maior de idade” ou “A pessoa é menor de idade”.
 // Bônus: Crie a condição utilizando operador ternário.
+// A estrutura de um operador ternário é compreendida da seguinte forma:
+// condição? valor se for verdareiro : valor se for falso
+
+function eMaiorDeIdadae (name, age) {
+
+let maioridade = age > 18 ? true : false  
+if (maioridade == true) {
+console.log (name + " é maior de idade")
+} else { 
+    console.log (name + " não é maior de idade")
+}
+}
+
+eMaiorDeIdadae ("Amanda",10)
+
 // 5. Crie um algoritmo que recebe a idade de Ademar, Adriana e Julia e imprima quem é a mais nova no formato:
 // “Pessoa” é a mais nova.  
+
+// let pessoas = {
+//     nome: ["Ademar", "Adriana", "Julia"],
+//     idades: [31,43,15]
+// }   a resolução que me vem a cabeça é com if, mas quero tentar desenvolver uma resolução com objeto
+
 
 
 
