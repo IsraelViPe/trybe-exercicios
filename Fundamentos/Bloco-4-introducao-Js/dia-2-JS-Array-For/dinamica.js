0b7c88bcd63a439b9bd39663afcf089bacbe3d60
IsraelPereira <pereira.israel2070@gmail.com>
2022-06-26 10:18:29 -0300

    altera resolução do exercício 2. Menos código. Eu preciso entender melhor o enunciado para gastar menos código.

--- Fundamentos/Bloco-4-introducao-Js/dia-2-JS-Array-For/dinamica.js
+++ Fundamentos/Bloco-4-introducao-Js/dia-2-JS-Array-For/dinamica.js
@@ -1,58 +1,28 @@
 // 1. Faça um algoritmo que calcule a soma de 35 a 105 usando a estrutura “for” e retorne no formato:
 // A soma de 35 a 105 é: X.
-// let array = [];
-let soma = 0;
-for ( let index = 35; index <= 36; index += 1) {
-    soma += index
 
+let soma = 0;
+for ( let index = 35; index <= 105; index += 1) {
+    soma += index
 }
-console.log(soma);
-// let soma = 0;
-// for (let index = 0; index < array.length; index += 1 ) {
-//     soma += array[index]
-// }
-// console.log(soma);
+console.log(" A soma de 35 a 105 é " + soma);
+
 
 // 2. Crie um algoritmo que conte quantos números do intervalo entre 15 e 155 são divisíveis por 3.
 // Caso a quantidade de números impares seja igual ou maior que 50, exiba uma mensagem secreta a sua escolha.
 
-// let array = [];
-// for ( let index = 15; index <= 155; index += 1) {
-//     array.push(index)
-//    }
-//    console.log(array)
-//    let numerosDiv3 = [];
-//    for ( let index = 0; index < array.length; index += 1) {
-//        if ( array[index] % 3 === 0) {
-//            numerosDiv3.push(array[index]);
-//        }
-//    }
-//    console.log(numerosDiv3);
-//    let numImpares = [];
-//    for (let index= 0 ; index < numerosDiv3.length; index += 1) {
-//        if ( numerosDiv3[index] % 2 !== 0){
-//            numImpares.push(numerosDiv3[index])
-//        }
-//        }
-//        if (numImpares.length <= 50) {
-//            console.log ("top secret")
-//        }
+let numerosDivisiveisPor3Impares = [];
+for ( let index = 15; index <= 155; index += 1) {
+    if ( index % 3 === 0 && index % 2 !== 0 ) {
+        numerosDivisiveisPor3Impares.push(index)
+    }
+    console.log(numerosDivisiveisPor3Impares)
+   }
+   if ( numerosDivisiveisPor3Impares.length >= 50) {
+    console.log ("top secret")
+   }
+  
 
-// let array = [];
-// for ( let index = 15; index <= 155; index += 1) {
-//  array.push(index)
-// }
-// console.log(array)
-// let numerosDiv3 = [];
-// for ( let index = 0; index < array.length; index += 1) {
-//     if ( array[index] % 3 === 0 && array[index] % 2 !== 0) {
-//         numerosDiv3.push(array[index]);
-//     }
-// }
-// console.log(numerosDiv3)
-// if (numerosDiv3.length <= 50) {
-//     console.log ("top secret")
-// }
 
 
 // 3. Crie um algoritmo que simula o jogo “pedra, papel e tesoura” levando em consideração  que são apenas duas pessoas jogando e imprima o resultado no formato:
