4df06d0746953135f15a9054f8da186bd35f24d7
IsraelPereira <pereira.israel2070@gmail.com>
2022-07-01 08:41:41 -0300

    adiciona exercicios

--- Fundamentos/Bloco-5-Js-Dom-Eventos-Web-Storage/dia-03-Eventos/script2.js
+++ Fundamentos/Bloco-5-Js-Dom-Eventos-Web-Storage/dia-03-Eventos/script2.js
@@ -12,6 +12,8 @@ function createDaysOfTheWeek() {
   };
   
   createDaysOfTheWeek();
+
+  // exercicio 1
   
   let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
@@ -39,6 +41,68 @@ for (let i = 0; i < decemberDaysList.length; i += 1) {
     tagLiDays.className = "day";
   }
   
-  
   paiDays.appendChild(tagLiDays);
 }
+
+// exercício 2
+
+// criar função que vai criar uma tag bottom, inserir id nela, inserir innerText
+// o appendChild deve ser feito com getElementsByClassName("buttons-container")[];
+const paiButton = document.getElementsByClassName("buttons-container")[0];
+
+function criaButtons (id,text) {
+  const newbutton = document.createElement("button");
+  newbutton.id = id;
+  newbutton.innerText = text;
+  return newbutton
+}
+paiButton.appendChild(criaButtons("btn-holiday",'Feriados'));
+
+// exercício 3
+
+// fluxo >>> O que essa função fará objetivamente ? 
+//  mudar o background color das tags (li)s que possuem class "holiday";
+// eu preciso de um for porque o getElementsByClassName me 
+// retornara um HTMLcolection
+// a função não necessita de parametro poi será usada em 
+// outro contexto, ela só precisa realizar a tarefa
+
+// fluxo da criação do evento
+// >>>> primeiro eu recupero o botão por meio do id
+// depois eu utilizo o escutador e uso dentro dele um
+// evento de "click" associado a função backgroundHoliday
+
+// NÃO CONSEGUI RESOLVER ESSE SOZINHO
+// a solução proposta foi uma função que contem todas as variáveis e 
+// o addEvent que por sua vez traz outra função, onde entram o for e um
+// if para  condicionar a alteração do background color
+
+
+function backgroundHoliday () {
+  
+  let feriados = document.getElementsByClassName("holiday");
+  let buttonFeriado = document.getElementById("btn-holiday");
+  let backgroundColor = 'rgb(238,238,238)';
+  let novaCor = "white";
+
+  buttonFeriado.addEventListener('click', function() {
+  for (let i = 0; i < feriados.length; i += 1) {
+    if ( feriados[i].style.backgroundColor === novaCor) {
+      feriados[i].style.backgroundColor = backgroundColor;
+    } else {
+      feriados[i].style.backgroundColor = novaCor;
+    }
+  }
+});
+}
+
+backgroundHoliday ();
+
+//  exercicio 4
+// fluxo >>> recupera local e chama função que você já fez
+
+const paiSextaButton = document.getElementsByClassName("buttons-container")[0];
+
+paiSextaButton.appendChild(criaButtons ("btn-friday","Sexta-feira"));
+
+
