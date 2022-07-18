b8c74cb8a3e235275821494b336d261cc7febebf
IsraelPereira <pereira.israel2070@gmail.com>
2022-07-18 20:36:45 -0300

    exercicio 3

---
+++ Fundamentos/Bloco-7-ES6-Teste-Unitarios/dia-03-Jest/exercicio3/myFizzBuzz.test.js
@@ -0,0 +1,17 @@
+const myFizzBuzz = require("./myFizzBuzz");
+const fizz = require("./myFizzBuzz");
+
+describe("testes da função myfizzBuzz", () => {
+  it("se o num for 15 o retorno deve ser fizzbuzz", () => {
+    expect("fizzbuzz").toBe(myFizzBuzz(15));
+  });
+  it("se o num for 3 o retorno deve ser fizz", () => {
+    expect("fizz").toBe(myFizzBuzz(3));
+  });
+  it("se o num for 5 o retorno deve ser buzz", () => {
+    expect("buzz").toBe(myFizzBuzz(5));
+  });
+  it("se o num for 2 o retorno deve ser 2 ", () => {
+    expect(2).toBe(myFizzBuzz(2));
+  });
+});
