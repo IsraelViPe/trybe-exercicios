ae458a656b735b89d9cf7e3561e301e98e30fe85
IsraelPereira <pereira.israel2070@gmail.com>
2022-07-26 13:31:09 -0300

    exercicio 1

---
+++ Fundamentos/Bloco-8-HOFs/dia-04-REDUCE/exercicio1.js
@@ -0,0 +1,10 @@
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+  ];
+  
+  function flatten(arr) {
+    return arr.reduce((acc,cur) => acc.concat(cur), [])
+  }
+ console.log(flatten(arrays)) ;
